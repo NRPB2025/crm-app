0c158d935595e34adb14c414b0a115c8807f72e2
NRPB2025 <nauzet.propelbon@gmail.com>
2025-05-12 10:27:08 +0100

    Update next.config.js

--- pages/next.config.js
+++ pages/next.config.js
@@ -1,10 +1,1 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    NEXT_PUBLIC_SUPABASE_KEY: process.env.NEXT_PUBLIC_SUPABASE_KEY
-  }
-}
 
-module.exports = nextConfig;
